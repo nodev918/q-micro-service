0908a8c343a9441029492481592b29a77eefe6fc
nodev918 <devnode77@gmail.com>
2021-10-03 23:34:09 +0800

    hh

---
+++ client/src/CommentList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default ({ comments }) =>{
+
+
+    const renderComments = comments.map(comment=>{
+        return <li key={comment.id}>{comment.content}</li>
+    })
+
+
+    return <ul>
+        {renderComments}
+    </ul>
+}
